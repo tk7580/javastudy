bf850df38912033734b839fb178bd69444110fb9
tk7580 <tkcode7580@gmail.com>
2025-03-07 16:39:10 +0900

    250307

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,53 +1,27 @@
 package org.example;
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a = 10;
 
-//        i++; => i 의 값을 1 증가 시킨다.
-//
-//        i--; => i 의 값을 1 감소 시킨다.
-//
-//        i = i + 2; => i 의 값을 2 증가 시킨다.
-//
-//        i += 2; => i 의 값을 2 증가 시킨다.(위와 같은 표현)
-//
-//        i = i - 2; => i 의 값을 2 감소 시킨다.
-//
-//        i -= 2; => i 의 값을 2 감소 시킨다.(위와 같은 표현)
+        int[] arr = new int[4];
 
-            //문제풀기
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = (i + 1) * 10;
+        }
 
-        //        double sum = 0;
-//        double i = -100;
-//        while (i <= 25){
-//            sum += i;
-//            i = i + 1;
-//        }
-//        System.out.println(sum);
-//
-//        double a = 1;
-//        double b = -1;
-//        double sum2 = 0;
-//
-//        while (a <= 10){
-//            while (b <= 3) {
-//                sum2 = sum2 + b;
-//                b = b + 1;
-//            }
-//            a = a + 1;
-//        }
-//        System.out.println(sum2);
-//
-//        int sum3 = 0;
-//        for (int o1 = 3; o1 < 1000; o1++){
-//            if ((o1 % 3 == 0) || (o1 % 5 == 0)){
-//                sum3 = sum3 + o1;
-//            }
-//        }
-//        System.out.println(sum3);
+        int sum = 0;
 
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+
+        int avg =sum / arr.length;
+
+        System.out.printf("총합 : %d\n", sum);
+
+        System.out.println("평균 : " + avg);
     }
 }
