c51ac9862d3822b293384874a78ebbc39f73afb3
tk7580 <tkcode7580@gmail.com>
2025-03-11 11:15:03 +0900

    overloading

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,22 @@ class Main {
         계산기.합( 30, 40); //출력 : 70
         계산기.합( 40, 30); //출력 : 70
 
+//        계산기.합(10, true);
+//        계산기.합(10, 40, 100);
+//        계산기.합(0);
+
+        계산기.합(0);
     }
 }
 
 class 계산기 {
+
+    static void 합(int c){
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+
     static void 합(int a, int b){
         System.out.println("a : " + a);
         System.out.println("b : " + b);
