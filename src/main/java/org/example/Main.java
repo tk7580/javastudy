e0e724015f8957518e28f6dcbc2da8bb14480e03
tk7580 <tkcode7580@gmail.com>
2025-03-11 09:34:45 +0900

    car

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,32 +4,41 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-
-        사람 a = new 사람();
-        a.age = 23;
-        a.name = "김철수";
-        a.isMarried = false;
-        a.introduce();
-
-        사람 b = new 사람();
-        b.age = 24;
-        b.name = "박영수";
-        b.isMarried = true;
-        b.introduce();
+//        문제 - 자동차 설계도를 만들어주세요.
+//        문제 - 자동차 객체를 담을 변수를 만들어주세요.
+//        문제 - 자동차 객체를 만들고 변수에 담아주세요.
+//        문제 - 자동차 객체마다 서로 다른 최고속력를 가지도록 해주세요.
+//        문제 - 1개의 자동차가 3번 달리게 해주세요.
+//        문제 - 객체를 사용하지 않고 두번째 플레이어를 만들어주세요.
+//        문제 - 3개의 자동차가 각각 1번씩 달리게 해주세요.
+//        문제 - 각각의 자동차가 서로 다른 최고속력으로 달리게 해주세요.
+//        문제 - 번호가 다른 각각의 자동차가 서로 다른 최고속력으로 달리게 해주세요.
         
+        자동차 a자동차1 = new 자동차();
+        자동차 a자동차2 = new 자동차();
+        자동차 a자동차3 = new 자동차();
+
+        a자동차1.번호 = 1;
+        a자동차2.번호 = 2;
+        
+        a자동차1.최고속력 = 100;
+        a자동차2.최고속력 = 200;
+        
+        a자동차1.달리다();
+        // 자동차가 100km/h로 달립니다.
+        a자동차2.달리다();
+        // 자동차가 200km/h로 달립니다
+        a자동차3.달리다();
     }
 }
 
-class 사람{
-    int age;
-    String name;
-    boolean isMarried = true;
-
-    void introduce(){
-        int age = this.age;
-        System.out.println("==자기소개==");
-        System.out.printf("이름 : %s\n", "김철수");
-        System.out.printf("나이 : %d\n", "age");
-        System.out.println("결혼여부 : %b\n", true);
+class 자동차 {
+    int 번호;
+    int 최고속력;
+    
+    void 달리다(){
+        System.out.println(this.번호 + "번 자동차가" + this.최고속력 + "km/h로 달립니다.");
     }
+    
+    
 }
