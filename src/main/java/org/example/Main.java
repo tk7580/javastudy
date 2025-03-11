ad3e16fd64f55452f33bff9e95e570ce3ecfcadb
tk7580 <tkcode7580@gmail.com>
2025-03-11 10:52:23 +0900

    parameter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,25 +4,21 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        //계산기.합();
 
-        Myclass a = new Myclass();
-        a.hi();
+        계산기.합( 10, 20); //출력 : 30
 
-        new Myclass().hi();
+        계산기.합( 30, 40); //출력 : 70
+        계산기.합( 40, 30); //출력 : 70
 
-//        hi();
-
-        funA();
-
-    }
-
-    static void funA() {
-        System.out.println("funA 실행됨");
     }
 }
 
-class Myclass{
-    void hi(){
-        System.out.println("hi 실행됨");
+class 계산기 {
+    static void 합(int a, int b){
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+        System.out.println(a+b);
     }
 }
