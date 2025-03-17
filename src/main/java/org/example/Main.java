fbff39afbe6fd83641256d75f7c2d64f17ae4e20
tk7580 <tkcode7580@gmail.com>
2025-03-17 15:34:10 +0900

    scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,96 +2,32 @@ package org.example;
 import org.w3c.dom.ls.LSOutput;
 import java.util.ArrayList;
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        v4();
-    }
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
 
-    static void v4() {
-        System.out.println("==v4==");
-        List
-                articles = new ArrayList<>();
-        articles.add(new Article()); // index : 0 , id : 1
-        articles.add(new Article()); // index : 1 , id : 2
-        articles.add(new Article()); // index : 2 , id : 3
+        //입력
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < articles.size(); i++) {
-            Article article = articles.get(i);
-            System.out.println(article.id);
+        int a = 0;
+
+        while(true){
+            System.out.printf("숫자 : ");
+            try{
+                a = sc.nextInt(); //대기, 숫자를 입력할 때까지
+                sc.nextLine(); //버퍼를 비운다
+                break;
+            } catch (InputMismatchException e){
+                sc.nextLine();
+                System.out.println("숫자 똑바로 써");
+            }
         }
-    }
 
-    static void v3() {
-        System.out.println("==v3==");
-        ArrayList
-                articles = new ArrayList<>();
-        articles.add(new Article()); // index : 0 , id : 1
-        articles.add(new Article()); // index : 1 , id : 2
-        articles.add(new Article()); // index : 2 , id : 3
-        articles.add(new Article()); // index : 3 , id : 4
+        System.out.printf("입력한 숫자 : %d\n", a);
 
-        for (int i = 0; i < articles.size(); i++) {
-            Article article = articles.get(i);
-            System.out.println(article.id);
-        }
-    }
-
-    static void v2() {
-        System.out.println("==v2==");
-        ArrayList articles = new ArrayList();
-        articles.add(new Article()); // index : 0 , id : 1
-        articles.add(new Article()); // index : 1 , id : 2
-        articles.add(new Article()); // index : 2 , id : 3
-        articles.add(new Article()); // index : 3 , id : 4
-
-        for (int i = 0; i < articles.size(); i++) {
-            Article article = (Article) articles.get(i);
-            System.out.println(article.id);
-        }
-    }
-
-    static void v1() {
-        System.out.println("==v1==");
-        Article[] articles = new Article[100];
-
-        int articlesSize = 0;
-
-        articles[0] = new Article();
-        articlesSize++;
-        articles[1] = new Article();
-        articlesSize++;
-        articles[2] = new Article();
-        articlesSize++;
-        articles[3] = new Article();
-        articlesSize++;
-        articles[4] = new Article();
-        articlesSize++;
-
-        for (int i = 0; i < articlesSize; i++) {
-            System.out.println(articles[i].id);
-        }
-    }
-}
-
-class Article {
-    static int lastId;
-
-    int id;
-    String regDate;
-
-    static {
-        lastId = 0;
-    }
-
-    Article() {
-        this(lastId + 1, "2025-12-12 12:12:12"); // 다른 생성자 호출(실행), Constructor Call
-        lastId++;
-    }
-
-    Article(int id, String regDate) {
-        this.id = id;
-        this.regDate = regDate;
+        sc.close();
     }
 }
