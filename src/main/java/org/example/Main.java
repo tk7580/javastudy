0cbd44a7ee313c39780e83828c0a557861af1227
tk7580 <tkcode7580@gmail.com>
2025-03-18 09:37:56 +0900

    stream

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,32 +2,26 @@ package org.example;
 import org.w3c.dom.ls.LSOutput;
 import java.util.ArrayList;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-
-        //입력
         Scanner sc = new Scanner(System.in);
 
-        int a = 0;
+        String[] inputs = sc.nextLine().split(" ");
 
-        while(true){
-            System.out.printf("숫자 : ");
-            try{
-                a = sc.nextInt(); //대기, 숫자를 입력할 때까지
-                sc.nextLine(); //버퍼를 비운다
-                break;
-            } catch (InputMismatchException e){
-                sc.nextLine();
-                System.out.println("숫자 똑바로 써");
-            }
-        }
+        long sum = Arrays.stream(inputs)
+                .mapToLong(e -> Long.parseLong(e))
+                .sum();
 
-        System.out.printf("입력한 숫자 : %d\n", a);
+//        long sum = 0;
+//
+//        for (String inputData : inputs){
+//            sum += Long.parseLong(inputData);
+//        }
 
-        sc.close();
+        System.out.println(sum);
     }
 }
